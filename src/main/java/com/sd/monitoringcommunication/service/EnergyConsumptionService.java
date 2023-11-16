c78e81a6363b01cb5fb0cd55e996741a5c33777f
Vlad Radu <mevladradu@gmail.com>
2023-11-16 23:51:35 +0200

    clean code EnergyConsumptionService

---
+++ src/main/java/com/sd/monitoringcommunication/service/EnergyConsumptionService.java
@@ -0,0 +1,35 @@
+package com.sd.monitoringcommunication.service;
+
+import com.sd.monitoringcommunication.dto.DeviceUpdateDTO;
+import com.sd.monitoringcommunication.model.EnergyConsumption;
+import com.sd.monitoringcommunication.repository.EnergyConsumptionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EnergyConsumptionService {
+    private final EnergyConsumptionRepository energyConsumptionRepository;
+
+    @Autowired
+    public EnergyConsumptionService(EnergyConsumptionRepository energyConsumptionRepository) {
+        this.energyConsumptionRepository = energyConsumptionRepository;
+    }
+
+    void updateEnergyConsumption(DeviceUpdateDTO data) {
+        energyConsumptionRepository.findByUsernameAndDeviceName(data.username(), data.deviceName())
+                .ifPresentOrElse(
+                        energyConsumption -> {
+                            energyConsumption.setMaxConsumption(data.maxConsumption());
+                            energyConsumptionRepository.save(energyConsumption);
+                        },
+                        () -> {
+                            EnergyConsumption energyConsumption = new EnergyConsumption(
+                                    data.username(),
+                                    data.deviceName(),
+                                    data.maxConsumption()
+                            );
+                            energyConsumptionRepository.save(energyConsumption);
+                        }
+                );
+    }
+}
--- src/main/java/com/sd/monitoringcommunication/service/KafkaListeners.java
+++ src/main/java/com/sd/monitoringcommunication/service/KafkaListeners.java
@@ -2,20 +2,17 @@ package com.sd.monitoringcommunication.service;
 
 import com.sd.monitoringcommunication.dto.DeviceUpdateDTO;
 import com.sd.monitoringcommunication.dto.MessageDTO;
-import com.sd.monitoringcommunication.model.EnergyConsumption;
-import com.sd.monitoringcommunication.repository.EnergyConsumptionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class KafkaListeners {
-
-    private final EnergyConsumptionRepository energyConsumptionRepository;
+    private final EnergyConsumptionService energyConsumptionService;
 
     @Autowired
-    public KafkaListeners(EnergyConsumptionRepository energyConsumptionRepository) {
-        this.energyConsumptionRepository = energyConsumptionRepository;
+    public KafkaListeners(EnergyConsumptionService energyConsumptionService) {
+        this.energyConsumptionService = energyConsumptionService;
     }
 
     @KafkaListener(topics = "monitoring", groupId = "group_id", containerFactory = "monitoringListenerContainerFactory")
@@ -27,20 +24,6 @@ public class KafkaListeners {
     void deviceListener(DeviceUpdateDTO data) {
         System.out.println("device updates: " + data);
 
-        energyConsumptionRepository.findByUsernameAndDeviceName(data.username(), data.deviceName())
-                .ifPresentOrElse(
-                        energyConsumption -> {
-                            energyConsumption.setMaxConsumption(data.maxConsumption());
-                            energyConsumptionRepository.save(energyConsumption);
-                        },
-                        () -> {
-                            EnergyConsumption energyConsumption = new EnergyConsumption(
-                                    data.username(),
-                                    data.deviceName(),
-                                    data.maxConsumption()
-                            );
-                            energyConsumptionRepository.save(energyConsumption);
-                        }
-                );
+        energyConsumptionService.updateEnergyConsumption(data);
     }
 }
